8a78bceefbb8c4b89f4a05cea07a30116c0fe01e
Fr0ntcoder <jambox228@gmail.com>
2024-07-04 18:56:08 +0800

    работа с юзером

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
 import { AuthController } from 'src/auth/auth.controller'
 import { AuthService } from 'src/auth/auth.service'
 import { JwtStrategy } from 'src/auth/jwt.strategy'
+import { getJwtConfig } from 'src/config/jwt.config'
 import { PrismaService } from 'src/prisma.service'
 import { UserService } from 'src/user/user.service'
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService, PrismaService, UserService, JwtStrategy],
+  imports: [
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJwtConfig,
+    }),
+  ],
 })
 export class AuthModule {}
