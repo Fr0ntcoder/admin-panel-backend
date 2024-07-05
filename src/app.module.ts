a6431485f857722fcb05379f14f7c0f1fd62204d
Fr0ntcoder <jambox228@gmail.com>
2024-07-05 19:23:38 +0800

    статистика

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { UserModule } from './user/user.module';
 import { SettingsModule } from './settings/settings.module';
+import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, SettingsModule],
+  imports: [ConfigModule.forRoot(), UserModule, SettingsModule, StatisticsModule],
 })
 export class AppModule {}
