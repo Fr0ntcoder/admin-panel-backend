4e5cd3381bc45ffad189d9ac6ad6a060f9b9d8b2
Fr0ntcoder <jambox228@gmail.com>
2024-07-08 15:35:56 +0800

    media

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
-import { MediaService } from './media.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { path } from 'app-root-path';
 import { MediaController } from './media.controller';
-
+import { MediaService } from './media.service';
 @Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: `${path}/uploads`,
+      serveRoot: '/uploads'
+    })
+  ],
   controllers: [MediaController],
   providers: [MediaService],
 })
